f2e5968c079be84c94a16d5ba642678bd27ea7af
StormShadowm <ma.morecruz@gmail.com>
2022-06-20 11:48:48 -0500

    push 2
    Arreglo de botones de redirección

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to=""></router-link> |
+    <router-link to="/listar">Listar</router-link> |
+    <router-link to="/crear">Crear</router-link> |
   </nav>
   <router-view />
 </template>
